c5702da1029aa52796a3c2171925f92d2c5d125d
Kenji Yamashita <s16t278@stu.kagawa-u.ac.jp>
2019-06-06 23:42:08 +0900

    show each weapon status

--- app/assets/javascripts/components/DropDownMenu.js.jsx
+++ app/assets/javascripts/components/DropDownMenu.js.jsx
@@ -12,7 +12,7 @@ class DropDownMenu extends React.Component {
         { name: "shotgun"},{ name: "scorestreak"},{ name: "sniper"},{ name: "specialist"},
         { name: "LMG"},{ name: "launcher"},{ name: "pistol"},{ name: "SMG"},{ name: "gear"},{ name: "weapon_melee"} 
       ],
-      weaponlistOpen: "null" 
+      weaponlistOpen: "null"
     }
   }
 
@@ -46,13 +46,10 @@ class DropDownMenu extends React.Component {
 
   render() {
     const {listOpen, eachlistOpen} = this.state
+    const weapons = []
 
-    function CheckWeapon(name) {
-      if (this.state.weaponlistOpen == name.groupname) {
-        return true
-      } else {
-        return false
-      }
+    function PushWeapon(val) {
+      return val
     }
     
     return (
@@ -60,57 +57,92 @@ class DropDownMenu extends React.Component {
         <div onClick={this.toggleList.bind(this)} style={styles.menuButton}>各武器</div>
         {listOpen &&
          (
-           <div>
-             {
-               this.state.weaponName.map(
-                 wname => 
-                   <div onClick={this.eachList.bind(this,wname.name)} style={styles.menuButton}>{wname.name}</div>
-               )
-             }
-             <table>
-               {eachlistOpen &&(
-                 <div>
-                   <thead>
-                     <tr>
-                       <th>Weapon</th>
-                       <th>Kill</th>
-                       <th>Death</th>
-                       <th>HeadShot</th>
-                       <th>Assist</th>
-                       <th>Ekia</th>
-                       <th>Accuracy</th>
-                       <th>TimeUsed</th>
-                     </tr>
-                   </thead>
-                   <tbody>                          
-                     {this.state.weaponStatus.map(
-                       eachweapon =>
-                         {  
-                           if(eachweapon.groupname == this.state.weaponlistOpen) {
-                             {console.log(eachweapon)}
-                             //<tr onClick={this.handleClickMenu.bind(this,eachweapon)}>
-                             <tr>
-                               <td>{eachweapon.name}</td>
-                               <td>{eachweapon.kill}</td>
-                               <td>{eachweapon.death}</td>
-                               <td>{eachweapon.headshot}</td>
-                               <td>{eachweapon.assist}</td>
-                               <td>{eachweapon.ekia}</td>
-                               <td>{eachweapon.accuracy}</td>
-                               <td>{eachweapon.timeused}</td>
-                             </tr>
-                           }
+         <div>
+           {
+             this.state.weaponName.map(
+               wname =>
+                 <div onClick={this.eachList.bind(this,wname.name)} style={styles.menuButton} key={wname.name}>{wname.name}</div>
+             )
+           }
+           
+           {
+             eachlistOpen && (
+               <div>
+                 {
+                   this.state.weaponStatus.forEach(
+                     getweapon =>
+                       {   
+                         if(getweapon.groupname == this.state.weaponlistOpen) {
+                           weapons.push(
+                             <ShowWeapon key={getweapon.id} showweapon={getweapon} />
+                           )
                          }
-                     )}
-                   </tbody>
-                 </div>
-               )}
-             </table>
-           </div>
-         )}
+                       }
+                   )
+                 }
+                 <StatusList weaponstatus={weapons} />
+               </div>
+             )
+           }
+         </div>
+         )
+        }
       </div>
     )
   }
+}
+const StatusList=(props)=>{
+  return(
+    <div>
+
+      <table className="weaponstatus">
+        <thead data-type="ok">
+          <tr>
+            <th>Weapon名</th>
+            <th>Kill数</th>
+            <th>Death数</th>
+            <th>HeadShot数</th>
+            <th>Assist数</th>
+            <th>Ekia</th>
+            <th>Accuracy</th>              
+            <th>TimeUsed</th>
+          </tr>            
+        </thead>          
+        <tbody>
+          {
+            props.weaponstatus
+            //<ShowWeapon showweapon={props.weaponstatus} />
+            //console.log(props.weaponstatus)
+            /*
+            props.weaponstatus.forEach(
+              (eachvalue) =>
+                {
+                  return <ShowWeapon showweapon={eachvalue.value} />   
+                }
+            )*/
+            
+          }
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+const ShowWeapon=(props)=>{
+  console.log(props.showweapon)
+  const {name, kill, death, headshot, assist, ekia, accuracy, timeused} = props.showweapon
+  return (
+        <tr>
+          <td>{name}</td>
+          <td>{kill}</td>
+          <td>{death}</td>
+          <td>{headshot}</td>
+          <td>{assist}</td>
+          <td>{ekia}</td>
+          <td>{accuracy}</td>
+          <td>{timeused}</td>
+        </tr>
+    )
 }
 
 const styles = {
--- app/assets/javascripts/components/WeaponStatus.js.jsx
+++ app/assets/javascripts/components/WeaponStatus.js.jsx
@@ -9,57 +9,11 @@ class WeaponStatus extends React.Component {
   }
   
   render() {
-//  <StatusList weaponStatus={this.state.weaponStatus} />  
-    
-      return (
-        <div>
-          <h1>各戦績</h1>
-          <DropDownMenu weaponStatus={this.state.weaponStatus} />
-
-        </div>
-      )
+    return (
+      <div>
+        <h1>各戦績</h1>
+        <DropDownMenu weaponStatus={this.state.weaponStatus} />
+      </div>
+    )
   }
 }
-
-const StatusList=(props)=> {
-  return (
-    <div>
-      <table className="weaponStatus">
-        <thead data-type="ok">
-          <tr>
-            <th>Weapon名</th>
-            <th>Kill数</th>
-            <th>Death数</th>
-            <th>HeadShot数</th>
-            <th>Assist数</th>
-            <th>Ekia</th>
-            <th>Accuracy</th>
-            <th>TimeUsed</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.weaponStatus.map((weaponStatus) =>
-                                  <StatusItem weaponStatus={weaponStatus} key={weaponStatus.id} />)}          
-        </tbody>
-      </table>
-    </div>
-  )
-}
-
-const StatusItem = (props) => {
-  const {name,kill, death, headshot, assist, ekia, accuracy, timeused} = props.weaponStatus
-  
-  return (
-    <tr>
-      <td>{name}</td>
-      <td>{kill}</td>
-      <td>{death}</td>
-      <td>{headshot}</td>
-      <td>{assist}</td>
-      <td>{ekia}</td>
-      <td>{accuracy}</td>
-      <td>{timeused}</td>
-    </tr>
-  )
-}
-
